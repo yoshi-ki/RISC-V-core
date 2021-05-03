795ad0020738b81b98be08039ad8a85640d7371d
藤原良樹 <yoshikif98@gmail.com>
2021-05-03 16:16:32 +0900

    finish executer

--- practice-files/ver1/executer.sv
+++ practice-files/ver1/executer.sv
@@ -3,15 +3,23 @@
 module executer(
   input wire CLK,
   input control_info CTR_INFO,
+  input wire [31:0] RS1_VAL,
+  input wire [31:0] RS2_VAL,
 
-  output
+  output wire [31:0] RESULT
+);
+
+wire [31:0] alu_result;
+alu aluer(
+  .CLK(CLK),
+  .RS1_VAL(RS1_VAL),
+  .RS2_VAL(RS2_VAL),
+  .ALU_RESULT(alu_result)
 );
 
 
 always @(posedge CLK) begin
-
-
-
+  RESULT <= alu_result;
 end
 
 end module
