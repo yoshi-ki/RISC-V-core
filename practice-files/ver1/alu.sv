6c533f1ccbe9583ab6339452b110fce9cb072e18
yoshi-ki <fujiwara@cad000.local>
2021-05-03 20:50:53 +0900

    increase the number of instructions

--- practice-files/ver1/alu.sv
+++ practice-files/ver1/alu.sv
@@ -11,8 +11,16 @@ module aluer(
 );
 
 wire [31:0] result =
-  CTR_INFO.add ? $signed(RS1_VAL) + $signed(RS2_VAL):
-  CTR_INFO.sub ? $signed(RS1_VAL) - $signed(RS2_VAL):
+  CTR_INFO.add  ? $signed(RS1_VAL) + $signed(RS2_VAL):
+  CTR_INFO.sub  ? $signed(RS1_VAL) - $signed(RS2_VAL):
+  CTR_INFO.sll  ? RS1_VAL << RS2_VAL:
+  CTR_INFO.slt  ? (($signed(RS1_VAL) < $signed(RS2_VAL)) ? 1 : 0):
+  CTR_INFO.sltu ? ((RS1_VAL < RS2_VAL) ? 1 : 0):
+  CTR_INFO.xor_ ? RS1_VAL ^ RS2_VAL:
+  CTR_INFO.srl  ? RS1_VAL >> RS2_VAL[4:0]:
+  CTR_INFO.sra  ? $signed(RS1_VAL) >>> RS2_VAL[4:0]:
+  CTR_INFO.or_  ? RS1_VAL | RS2_VAL:
+  CTR_INFO.and_ ? RS1_VAL & RS2_VAL:
   31'b0;
 
 
