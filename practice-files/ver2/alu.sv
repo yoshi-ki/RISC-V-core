5c771f66d99f56a774ec3cdab85c801125f4271c
yoshi-ki <fujiwara@cad000.local>
2021-05-21 13:37:35 +0900

    instructions using immediate

--- practice-files/ver2/alu.sv
+++ practice-files/ver2/alu.sv
@@ -11,6 +11,30 @@ module aluer(
 );
 
 wire [31:0] result =
+  // instructions lui, auipc,
+  // TODO: implement instructions
+
+  // jump instructions
+  // TODO: implement instructions
+
+  // branch instructions
+  // TODO: implement instructions
+
+  // instructions for load and store
+  // TODO: implement instructions
+
+  // instructions using immediate and registers
+  CTR_INFO.addi  ? $signed(RS1_VAL) + $signed(CTR_INFO.immediate):
+  CTR_INFO.slti  ? (($signed(RS1_VAL) < $signed(CTR_INFO.immediate)) ? 1 : 0):
+  CTR_INFO.sltiu ? ((RS1_VAL < CTR_INFO.immediate) ? 1 : 0):
+  CTR_INFO.xori  ? RS1_VAL ^ CTR_INFO.immediate:
+  CTR_INFO.ori   ? RS1_VAL | CTR_INFO.immediate:
+  CTR_INFO.andi  ? RS1_VAL & CTR_INFO.immediate:
+  CTR_INFO.slli  ? RS1_VAL << CTR_INFO.immediate[4:0]:
+  CTR_INFO.srli  ? RS1_VAL >> CTR_INFO.immediate[4:0]:
+  CTR_INFO.srai  ? RS1_VAL >> CTR_INFO.immediate[4:0]:
+
+  // instructions using two registers
   CTR_INFO.add  ? $signed(RS1_VAL) + $signed(RS2_VAL):
   CTR_INFO.sub  ? $signed(RS1_VAL) - $signed(RS2_VAL):
   CTR_INFO.sll  ? RS1_VAL << RS2_VAL:
@@ -21,7 +45,7 @@ wire [31:0] result =
   CTR_INFO.sra  ? $signed(RS1_VAL) >>> RS2_VAL[4:0]:
   CTR_INFO.or_  ? RS1_VAL | RS2_VAL:
   CTR_INFO.and_ ? RS1_VAL & RS2_VAL:
-  31'b0;
+  32'b0;
 
 
 always @(posedge CLK) begin
---
+++ practice-files/ver2/xsim.dir/test_cpu.sim/xsim_script.tcl
@@ -0,0 +1,1 @@
+xsim {test_cpu.sim} -autoloadwcfg -runall
